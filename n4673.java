6fc6dedf02589023f562f576a3bd3b0ddaadee9d
jini_11 <jieun21124@naver.com>
2021-02-10 08:55:02 +0900

    correct

--- n4673.java
+++ n4673.java
@@ -2,26 +2,31 @@ public class n4673 {
     static boolean[] result = new boolean[10001];
 
     public static void main(String[] args) {
-        get_selfnum();
+        for(int i=1;i<result.length;i++){
+
+            get_selfnum(i);
+
+        }
+        for(int i=1;i<result.length;i++){
+            if(result[i]==false)
+            System.out.println(i);
+        }
 
     }
 
     //1부터  1000까지 각 자리수를 더한 값에 위치한 result를 true로 변경하기-by 지은
-    public static void get_selfnum() {
-        for(int i=1;i<10001;i++){
+    public static void get_selfnum(int i) {
+        
             int sum=i;
             sum+=i/1000;
             sum+=(i%1000)/100;
-            sum+=(i%1000)%100/10;
+            sum+=((i%1000)%100)/10;
             sum+=(i%1000)%100%10;
 
             if(sum<10001)
             result[sum]=true;
 
-        }
-        for(int i=0;i<10001;i++){
-            if(result[i]==false)
-            System.out.println(i);
-        }
+        
+       
     }
 }
