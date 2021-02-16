d3874a2dcf6660ee0a25a2535f55780b5cc8c444
jini_11 <jieun21124@naver.com>
2021-02-16 19:01:31 +0900

    20210216

--- n1011.java
+++ n1011.java
@@ -5,17 +5,33 @@ public class n1011 {
         Scanner sc=new Scanner(System.in);
         
         int test=sc.nextInt();
+       
         for(int i=0;i<test;i++){
             int x=sc.nextInt();
             int y=sc.nextInt();
-            int count=0;
 
-            count=(y-x-2)/2;
-            count+=(y-x-2)%2;
-            count+=2;
-
+            int distance=y-x-2;
+            int count=2;
+            int jump=1;
+           
+            while(true){
+                if(distance==0)
+                    break;
+                
+               
+                
+                if(distance<0){
+                    distance+=jump+1;
+                    jump-=1;
+                    count--;
+                }
+                else{
+                    distance-=(jump+1);
+                    count++;
+                    jump++;
+                }
+            }
             System.out.println(count);
-
         }
     }
     
--- n2839.java
+++ n2839.java
@@ -7,6 +7,23 @@ public class n2839 {
         
         int n=Integer.parseInt(br.readLine());
         int count=0;
+
+        while(true){
+            if(n%5==0){
+                count+=n/5;
+                System.out.println(count);
+                break;
+            }
+            else{
+                n-=3;
+                count++;
+            }
+            if(n<0){
+                System.out.println(-1);
+                break;
+            }
+        }
+
        
     }
 }
