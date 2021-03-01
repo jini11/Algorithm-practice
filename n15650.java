2af296ff90c30275b534356f13158d0163d425a3
jini_11 <jieun21124@naver.com>
2021-03-01 21:46:30 +0900

    20210301

---
+++ n15650.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class n15650 {
+    static int[] result;
+    static boolean[] key;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        result = new int[m];
+        key=new boolean[n];
+
+        dfs(n,m,0);
+    }  
+    public static void dfs(int n,int m,int count){
+        
+        if(count==m){
+            for(int i=0;i<m;i++){
+                System.out.print(result[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=0;i<n;i++){
+            if(key[i])
+                continue;
+            else{
+                key[i]=true;
+                result[count]=i+1;
+                dfs(n,m,count+1);
+                key[i]=false;
+            }
+            
+        }
+        
+    }
+}
