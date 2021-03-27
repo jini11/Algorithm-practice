7f13521debc7d48573dad5afa5821728e8250a8d
jini_11 <jieun21124@naver.com>
2021-03-28 01:04:21 +0900

    20210328

--- n2609.java
+++ n2609.java
@@ -1,36 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class n2609 {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
 
-        // int m=Integer.parseInt(br.readLine().split(" ")[0]);
-        // int n=Integer.parseInt(br.readLine().split(" ")[1]);
-        int m = sc.nextInt();
+         int m = sc.nextInt();
         int n = sc.nextInt();
-        int great = 1;
-
-        int max = Math.max(m, n);
+        int min = 1;
+        int mx=m;
+        int nx=n;
+        
         int i=2;
         while (true) {
-            if(i==max)
+            if(i>m && i>n)
                 break;
 
             if (m % i == 0 && n % i == 0) {
-                great *= i;
+                min *= i;
                 m = m / i;
                 n = n / i;
                 i = 2;
-                max=Math.max(m,n);
+                
             }
-            i++; 
+            else
+              i++; 
         }
 
-        System.out.println(great);
-        System.out.println(great * m * n);
+        System.out.println(min);
+        System.out.println(mx*nx/min); //최소공배수 공식
 
     }
 }
+
