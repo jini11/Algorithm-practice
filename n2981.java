756d0b6fd56116960e3e966abe1a90a7e4b5481e
jini_11 <jieun21124@naver.com>
2021-03-29 16:04:47 +0900

    20210329

--- n2981.java
+++ n2981.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class n2981 {
     public static void main(String[] args) throws Exception {
@@ -8,9 +9,29 @@ public class n2981 {
         int N = Integer.parseInt(br.readLine());
 
         int[] arr = new int[N];
+       
+        int min = 0;
+
         for (int i = 0; i < N; i++) {
-            arr[i]=Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        
+        Arrays.sort(arr);
+        min = arr[0];
+        int[] num = new int[min];
+        for (int i = 2; i < min; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (arr[j] % i == arr[k] % i) {
+                        num[i]++;
+                    }
+                }
+            }
+            if (num[i] < N - 1)
+                num[i] = 0;
+
+            if (num[i] != 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
