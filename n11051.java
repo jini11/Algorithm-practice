3ccc88983be63e435ab29e89a27dd58ebda41503
jini_11 <jieun21124@naver.com>
2021-03-30 15:53:40 +0900

    20210330

--- n11051.java
+++ n11051.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 
 public class n11051 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-       // int result=1;
+    public static int[][] arr;
 
-      //   result=factorial(a)/(factorial(b)*factorial(a-b));
-        System.out.println(factorial(a)/(factorial(b)*factorial(a-b))%10007);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        arr = new int[a + 1][a + 1];
+   
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<=i;j++){
+                if(i==j || j==0)
+                    arr[i][j]=1;
+                else{
+                    arr[i][j]=(arr[i-1][j-1]+arr[i-1][j])%10007;
+                }
+            }
+        }
+        System.out.println(arr[a][b]);
+     
     }
-    public static int factorial(int n){
-        if(n==0)
-            return 1;
-        return n*factorial(n-1);
-    }
+
+    
 }
+
+/*
+ * 
+ * 1 1 1 1 2 1 1 3 3 1 1 4 6 4 1
+ */
