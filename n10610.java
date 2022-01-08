62cf4c963ad26676f3061e42858809ac78fcfb45
jini_11 <jieun21124@naver.com>
2022-01-08 12:00:18 +0900

    다시풀기

--- n10610.java
+++ n10610.java
@@ -14,12 +14,14 @@ public class n10610 {
 
         Arrays.sort(num);
 
+        int sum = 0;
         for(int i=0;i<num.length;i++) {
+            sum += num[i];
             long temp = (long) (num[i] * Math.pow(10, i));
             result += temp;
         }
 
-        if(result%30==0&&n.contains("0"))
+        if(sum%3==0&&n.contains("0"))
             bw.write(result+"\n");
         else
             bw.write("-1\n");
@@ -29,3 +31,9 @@ public class n10610 {
         bw.close();
     }
 }
+
+//내가 사용한 방법
+// 입력값을 내림차순으로 정렬 후 30으로 나누어 떨어지면 성공
+
+//그 외
+// 0이 포함되고, 각 숫자의 합이 3의 배수이면 성공
