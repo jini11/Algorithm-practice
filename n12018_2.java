27a3282f228d8e5c6bdd5e75bd3694487e4be6bd
jini_11 <jieun21124@naver.com>
2022-02-08 14:08:53 +0900

    20220208

--- n12018_2.java
+++ n12018_2.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
 
-public class n12018 {
+public class n12018_2 {
     public static void main(String []args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String temp = br.readLine();
         int n = Integer.parseInt(temp.split(" ")[0]);
         int m = Integer.parseInt(temp.split(" ")[1]);
-        int[] result = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int count = 0;
         for(int i=0;i<n;i++) {
             String num = br.readLine();
@@ -21,14 +22,14 @@ public class n12018 {
                 score[j] = Integer.parseInt(s.split(" ")[j]);
             if(sub>=full) {
                 Arrays.sort(score, Collections.reverseOrder());
-                result[i] = score[full-1];
+                pq.add(score[full-1]);
             }
-            else result[i] = 1;
+            else pq.add(1);
         }
-        Arrays.sort(result);
+        
         int sum = 0;
         for(int i=0;i<n;i++) {
-            sum += result[i];
+            sum += pq.remove();
             if(sum > m) break;
             count++;
         }
