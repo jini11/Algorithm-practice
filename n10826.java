081608c2757d2a76d4cb939d5917b17f0acbbd73
jini_11 <jieun21124@naver.com>
2022-03-20 15:18:00 +0900

    20220320

--- n10826.java
+++ n10826.java
@@ -7,7 +7,7 @@ public class n10826 {
     public static void main(String []args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        BigInteger[] arr = new BigInteger[n+1];
+        BigInteger[] arr = new BigInteger[n+2];
         arr[0] = BigInteger.ZERO;
         arr[1] = BigInteger.ONE;
         for(int i=2;i<=n;i++)
