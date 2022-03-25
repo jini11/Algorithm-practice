67fbeb908b34583865f5fd16280e0a29e8a6b709
jini_11 <jieun21124@naver.com>
2022-03-25 15:10:46 +0900

    20220325

--- n1932.java
+++ n1932.java
@@ -1,23 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
+import java.util.Scanner;
 
 public class n1932 {
-    public static int[][] arr=new int[5][];
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n=Integer.parseInt(br.readLine());
-        for(int i=0;i<n;i++){
-            String[] str=br.readLine().split(" ");
-            for(int j=0;j<n;j++){
-                arr[i][j]=Integer.parseInt(str[j]);
+    static int[][] arr;
+    static int[][] count;
+    static int n;
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n][n];
+        count = new int[n][n];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<=i;j++) {
+                arr[i][j] = sc.nextInt();
+                if(i==n-1) {
+                    count[i][j] = arr[i][j];
+                }
             }
         }
-
-        //System.out.println(triangle(0,0));
+        System.out.println(triangle(0, 0));
     }
-    //public static int triangle(int n,int m){
-        
-    //}
+    public static int triangle(int x, int y) {
+        if(x == n-1) return count[x][y];
+        if(count[x][y] == 0) {
+            count[x][y] = Math.max(triangle(x+1, y), triangle(x+1, y+1)) + arr[x][y];
+        }
+        return count[x][y];
+    }
 }
