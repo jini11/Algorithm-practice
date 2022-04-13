b05882e952a6096284976d95e296d41dd1531e0c
jini_11 <jieun21124@naver.com>
2022-04-13 11:53:53 +0900

    20220413

--- n16236.java
+++ n16236.java
@@ -1,21 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
-public class n16236 {
+public class n16236 { // 다시 풀어볼 것, 거리 수 세는 거 주의
     static int[][] arr;
     static boolean[][] visited;
     static int n, count, bx, by;
     static int size = 2;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static int[] dx = {0, -1, 1, 0};
+    static int[] dy = {-1, 0, 0, 1};
     public static void main(String []args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
-        visited = new boolean[n][n];
         count = 0;
         for(int i=0;i<n;i++) {
             String line = br.readLine();
@@ -28,31 +26,47 @@ public class n16236 {
                 }
             }
         }
-        bfs(bx, by);
-        System.out.println(count);
-    }
-    public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        visited[x][y] = true;
-        queue.add(new int[] {x, y});
-        while(!queue.isEmpty()) {
-            int[] point = queue.poll();
-            for(int i=0;i<4;i++) {
-                int nx = point[0] + dx[i];
-                int ny = point[1] + dy[i];
-                if(nx<0 || ny<0 || nx>=n || ny>=n || size<arr[nx][ny]) continue;
-                if(!visited[nx][ny]) {
-                    if(size == arr[nx][ny]) 
-                        queue.add(new int[] {nx, ny});
-                    else if(size > arr[nx][ny]) {
-                        arr[nx][ny] = 0;
-                        size += arr[nx][ny];
-                        queue.add(new int[] {nx, ny});
-                    }
+        
+        int eat = 0;
+        int[] cnt = null;
+        cnt = new int[] {bx, by};
+        while(true) {
+            PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) ->
+            o1[2] != o2[2] ? Integer.compare(o1[2], o2[2]) : (o1[0] != o2[0] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]))
+            );
+            visited = new boolean[n][n];
+            queue.add(new int[] {cnt[0], cnt[1], 0});
+            visited[cnt[0]][cnt[1]] = true;
+
+            boolean key = false;
+
+            while(!queue.isEmpty()) {
+                cnt = queue.poll();
+                if(arr[cnt[0]][cnt[1]] != 0 && arr[cnt[0]][cnt[1]] < size) {
+                    eat++;
+                    arr[cnt[0]][cnt[1]] = 0;
+                    count += cnt[2];
+                    key = true;
+                    break;
+                }
+                for(int i=0;i<4;i++) {
+                    int nx = cnt[0] + dx[i];
+                    int ny = cnt[1] + dy[i];
+                    if(nx<0 || ny<0 || nx>=n || ny>=n || visited[nx][ny] || arr[nx][ny] > size) continue;
+                    queue.add(new int[] {nx, ny, cnt[2]+1});
                     visited[nx][ny] = true;
-                    count++;
                 }
             }
+
+            if(!key) break;
+            if(size == eat) {
+                eat = 0;
+                size++;
+            }
         }
+       
+        
+        System.out.println(count);
     }
+    
 }
