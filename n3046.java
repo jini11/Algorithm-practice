c0e52e82e124ef139187f794df30b0423686cc5e
jini_11 <jieun21124@naver.com>
2022-04-27 23:10:53 +0900

    20220427

---
+++ n3046.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class n3046 {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+        System.out.println(s*2 - r1);
+    }
+}
