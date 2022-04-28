ca474407b00cd2d67627bf2be1b9d7265baf0a80
jini_11 <jieun21124@naver.com>
2022-04-28 21:03:44 +0900

    20220428

--- n2455.java
+++ n2455.java
@@ -4,14 +4,14 @@ public class n2455 {
     public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[4][2];
+        int count = 0;
+        int result = 0;
         for(int i=0;i<4;i++) {
             arr[i][0] = sc.nextInt();
             arr[i][1] = sc.nextInt();
+            count = count - arr[i][0] + arr[i][1];
+            result = Math.max(result, count);
         }
-        int count = arr[0][1];
-        for(int i=1;i<4;i++) {
-            count = Math.max(count, count-arr[i][0]+arr[i][1]);
-        }
-        System.out.println(count);
+        System.out.println(result);
     }
 }
