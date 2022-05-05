cabb208f5f71ad1fa3776f6d067c03849f00e27f
jini_11 <jieun21124@naver.com>
2022-05-05 17:09:08 +0900

    20220505

--- n2580.java
+++ n2580.java
@@ -1,78 +1,63 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class n2580 {
-    static int[][] arr=new int[9][9];
-    static boolean check_row[]=new boolean[10]; //가로
-    static boolean check_col[]=new boolean[10]; //세로
-    static boolean check_square[]=new boolean[10]; //3*3;
-    static int count=0;
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                arr[i][j]=sc.nextInt();
+    static int[][] map = new int[9][9];
+    public static void main(String []args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        for(int i=0;i<9;i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<9;j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        System.out.println();
-
-        puzzle(0,0);
-
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-        
-
+        puzzle(0, 0);
     }
-    public static void puzzle(int x,int j){
-
-        if(x==9){
-            puzzle(x+1,0);
+    public static void puzzle(int x, int y) { // 재귀함수이자, 1~9 넣기
+        if(y==9) {
+            puzzle(x+1, 0);
             return;
-        }         
-
-        for(j=0;j<9;j++){
-            if(arr[x][j]==0){
-                for(int k=0;k<9;k++){//가로 세로
-                    check_row[arr[k][j]]=true;
-                    check_col[arr[x][k]]=true;
-                
-                }
-               
-                for(int m=(x/3)*3;m<(x/3)*3+3;m++){
-                    for(int n=(j/3)*3;n<(j/3)*3+3;n++){//3*3
-                        check_square[arr[m][n]]=true;
-                        
-                    }
-                }
-               
-                for(int k=0;k<10;k++){//다 false면 0대신 인덱스 넣어주기
-                    if(check_row[k]==false && check_col[k]==false && check_square[k]==false){
-                        arr[x][j]=k;
-                        //puzzle(x,j);
-                       // arr[x][j]=0;
-                    }
-                    
-                }
-                
-               // puzzle(x,j+1);
-
-                for(int k=0;k<10;k++){
-                    check_row[k]=false;
-                    check_col[k]=false;
-                    check_square[k]=false;
-
-                }
-                //puzzle(x+1,j);
-                
+        }
+        if(x==9) {
+            for(int i=0;i<9;i++) {
+                for(int j=0;j<9;j++)
+                    System.out.print(map[i][j]+" ");
+                System.out.println();
             }
-            count++;
-            
+            System.exit(0);
         }
 
+        if(map[x][y]==0) {
+            for(int i=1;i<=9;i++) {
+                if(check(x, y, i)) {
+                    map[x][y] = i;
+                    puzzle(x, y+1);
+                }
+            }
+            map[x][y] = 0;
+            return;
+        }
+        puzzle(x, y+1);
     }
+    public static boolean check(int x, int y, int num) { // 가로줄, 세로줄, 3x3 검사 후 결과 반환
+        for(int i=0;i<9;i++) {
+            if(map[x][i] == num) 
+                return false;
+            if(map[i][y] == num)
+                return false;
+        }
+        int nx = x/3 * 3;
+        int ny = y/3 * 3;
+        for(int i=nx;i<nx+3;i++) {
+            for(int j=ny;j<ny+3;j++) {
+                if(map[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
+
 }
