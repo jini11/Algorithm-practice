b019c6c968ba54c25ffbc52a97e63a076b5196b4
jini_11 <jieun21124@naver.com>
2022-05-28 16:42:20 +0900

    20220528

---
+++ n11719.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n11719 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = "";
+		while((a=br.readLine()) != null)
+		{
+			System.out.println(a);
+		}
+	}
+} 
+
