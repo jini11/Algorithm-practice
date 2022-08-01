f9e654ba5ff7f2d681b8e22b33676fbbd978327a
jini_11 <jieun21124@naver.com>
2022-08-01 23:50:53 +0900

    20220801

--- n11286.java
+++ n11286.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
-
+ 
 public class n11286 {
     public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
