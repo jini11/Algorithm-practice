5abe7e9fda90f0268db3631c4390b7a425489d66
jini_11 <jieun21124@naver.com>
2022-09-02 14:20:10 +0900

    20220902

---
+++ n1912.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        int max = arr[0];
+        dp[0] = arr[0];
+        for(int i=1;i<n;i++) {
+            dp[i] = Math.max(arr[i], arr[i] + dp[i-1]);
+            if(max < dp[i]) max = dp[i];
+        } 
+        System.out.println(max);
+    }
+}
