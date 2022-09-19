c6f4cce1ceaa649fb58ec0e9452ba3836d2831d8
jini_11 <jieun21124@naver.com>
2022-09-19 15:00:25 +0900

    20220919

---
+++ n2444.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class n2444 {
+    public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		final int N = sc.nextInt();
+		
+		for(int i = 1; i <= N ; i++) {
+			for(int j = 0; j < N-i; j++)
+				System.out.print(" ");
+			for(int j = 0; j < i*2-1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+		
+		for(int i = N-1; i >= 0 ; i--) {
+			for(int j = 0; j < N-i; j++)
+				System.out.print(" ");
+			for(int j = 0; j < i*2-1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
